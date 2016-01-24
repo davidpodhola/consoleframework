df6f9c537e489af8f553b3b2f78ad7de32b3c306
David Podhola <david@podhola.net>
2016-01-24 22:20:35 +0100

    fewer bugs
    some just DAP commented out

---
+++ ConsoleFramework/ConsoleBeeper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleFramework
+{
+    public static class ConsoleBeeper
+    {
+        public static void Beep( this object console )
+        {
+
+        }
+    }
+}
